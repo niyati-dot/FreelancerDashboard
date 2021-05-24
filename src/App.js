ff2fce151870c940874ec6e1bb1c241a99029eb7
Deep_Prashantkumar_Patel <dp889845@dal.ca>
2021-05-24 12:36:15 -0300

    Added route to deep

--- src/App.js
+++ src/App.js
@@ -3,47 +3,44 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  useRouteMatch,
-  useParams
+  Link
 } from "react-router-dom";
 
 import './App.css'
 
 export default function App() {
   return (
-    <div>
-    <Router>
-      <div>
-        <ul id="menu">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">Authors
-            
-            </Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/about">
-          <ul>
-            <li><Link to="/">Vishal</Link></li>
-            <li><Link to="/">Janvi</Link></li>
-            <li><Link to="/">Deep</Link></li> 
-            <li><Link to="/">Bansi</Link></li> 
-            <li><Link to="/">Tejaswi</Link></li> 
-            <li><Link to="/">Sanket</Link></li> 
+    <div style = {{ marginLeft : 30 }}>
+      <Router>
+        <div>
+          <ul id="menu" style = {{ marginLeft : -60 }}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
           </ul>
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/bansi">
+              <h2>Hello!! This is Bansi!!</h2>
+            </Route>
+            <Route path="/deep">
+              <Deep />
+            </Route>
+            <Route path="/janvi">
+              <h1> Hello This is janvi </h1>
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
@@ -53,5 +50,25 @@ function Home() {
 }
 
 function About() {
-  return <h2>About</h2>;
+  return <div>
+    <h2>About</h2>
+    <p>We are group 5!</p>
+    <h4>Team</h4>
+    <ul>
+    <li><Link to="/bansi">Bansi</Link></li> 
+      <li><Link to="/deep">Deep</Link></li> 
+      <li><Link to="/janvi">Janvi</Link></li>
+      <li><Link to="/">Sanket</Link></li> 
+      <li><Link to="/">Tejaswi</Link></li> 
+      <li><Link to="/">Vishal</Link></li> 
+    </ul>
+  </div>
+  return 
+}
+
+function Deep(){
+  return <div>
+          <h2>Hey! This is me!</h2>
+          <p>Look my name on the URL!!</p>
+         </div>
 }
