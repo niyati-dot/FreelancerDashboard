361ea9753d5cbc2c9eb72f41a63a7b976db17c9a
Sanket Shah <sn288407@dal.ca>
2021-06-20 11:56:35 -0300

    Adding an event for Calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -2,18 +2,24 @@ import React from 'react';
 import PageHeader from "../components/PageHeader";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 export default function Calendar(){
     
     const events = [{ title: "today's event", date: new Date() }];
+
+    const handleDateClick = (arg) => {
+        var dateevent = prompt("Enter an event for date: "+arg.dateStr);
+    }
 
     return (
         <div>
             <PageHeader title="Calendar"/>
             <FullCalendar
                 defaultView="dayGridMonth"
-                plugins={[dayGridPlugin]}
+                plugins={[dayGridPlugin, interactionPlugin]}
                 events={events}
+                dateClick={handleDateClick}
             />
         </div>
     )
