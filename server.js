e22009a81dd79ddbb4229deef6f3a2e905171e85
Vishal Sancheti <vishky.sancheti@gmail.com>
2021-07-13 17:32:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './frontend/build')));
 app.use('/api',require('./api'));
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`App started at http://localhost:${port}`)
