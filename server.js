fa6f4ee7d64b46fdb86918ebdc62a1577bdb6085
Bansi Mehta <bn955101@dal.ca>
2021-07-21 17:22:20 -0300

    server.js: Mongodb connection changes

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path');
+const app = express();
+const cors = require('cors');
+let bodyParser = require("body-parser");
+const {MongoClient} = require('mongodb');
+const mongoUrl = "mongodb://ass3:2020@assignment3-shard-00-00.3zfwi.mongodb.net:27017,assignment3-shard-00-01.3zfwi.mongodb.net:27017,assignment3-shard-00-02.3zfwi.mongodb.net:27017/freelancer?ssl=true&replicaSet=atlas-7i888h-shard-0&authSource=admin&retryWrites=true&w=majority"
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, './frontend/build')));
-app.use('/api',require('./api'));
-app.get('/*', (req,res) => {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-});
+const todoListRouter = require('./api/routes/TodoListRoutes')
 
-app.listen(port, () => {
-    console.log(`App started at http://localhost:${port}`)
-});
+try {
+    mongoose.connect(mongoUrl,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+    });
+    
+    console.log("Connected DB Successfully")
+
+} catch (e) {
+    console.error(e);
+}
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'))
+app.use(cors())
+
+app.use('/todoLists', todoListRouter)
+
+app.listen(8080)
+console.log("Server Running")
