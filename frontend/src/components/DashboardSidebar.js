8c563d5de8b701dccd17ecd5c8bdf3d6dbdcbfff
Vishal Sancheti <vishky.sancheti@gmail.com>
2021-07-28 16:37:43 -0300

    added icons to sidemenu

--- frontend/src/components/DashboardSidebar.js
+++ frontend/src/components/DashboardSidebar.js
@@ -20,36 +20,52 @@ export default function Sidebar(props) {
                 <Drawer.Overflow>
                     <Drawer.ToC>
                         <Drawer.Item href="/dashboard">
-                            <span className={location.pathname === "/dashboard" ? 'nav-item-active' : ''}>Dashboard</span>
+                            <span className={location.pathname === "/dashboard" ? 'nav-item-active' : ''}>
+                                <i className="fas fa-tachometer-alt"></i> Dashboard
+                            </span>
                         </Drawer.Item>
 
                         <Drawer.Nav>
                             <Drawer.Item href="/clients">
-                                <span className={location.pathname === "/clients" ? 'nav-item-active' : ''}>Clients</span>
+                                <span className={location.pathname === "/clients" ? 'nav-item-active' : ''}>
+                                    <i className="fas fa-users"></i> Clients
+                                </span>
                             </Drawer.Item>
                             <Drawer.Item  href="/projects">
-                                <span className={location.pathname === "/projects" ? "nav-item-active" : ''}>Projects</span>
+                                <span className={location.pathname === "/projects" ? "nav-item-active" : ''}>
+                                    <i className="fas fa-briefcase"></i> Projects
+                                </span>
                             </Drawer.Item>
                             <Drawer.Item href="/timelogs">
-                                <span className={location.pathname === "/timelogs" ? 'nav-item-active' : ''}>Time Logs</span>
+                                <span className={location.pathname === "/timelogs" ? 'nav-item-active' : ''}>
+                                    <i className="fas fa-stopwatch"></i> Time Logs
+                                </span>
                             </Drawer.Item>
                             <Drawer.Item href="/invoicemanagement">
-                                <span className={location.pathname === "/invoicemanagement" ? 'nav-item-active' : ''}>Invoices</span>
+                                <span className={location.pathname === "/invoicemanagement" ? 'nav-item-active' : ''}>
+                                    <i className="fas fa-file-invoice"></i> Invoices
+                                </span>
                             </Drawer.Item>
                             <Drawer.Item href="/invoice-generation">
                                 <span className={location.pathname === "/invoice-generation" ? 'nav-item-active' : ''}>Invoice Generation</span>
                             </Drawer.Item>
                             <Drawer.Item href="/testimonials">
-                                <span className={location.pathname === "/testimonials" ? 'nav-item-active' : ''}>Testimonials</span>
+                                <span className={location.pathname === "/testimonials" ? 'nav-item-active' : ''}>
+                                    <i className="fas fa-comment"></i> Testimonials
+                                </span>
                             </Drawer.Item>
 
                             <hr className="horizontal-break"/>
 
                             <Drawer.Item href="/todolist">
-                                <span className={location.pathname === "/todolist" ? 'nav-item-active' : ''}>ToDo</span>
+                                <span className={location.pathname === "/todolist" ? 'nav-item-active' : ''}>
+                                    <i className="fas fa-tasks"></i> ToDo
+                                </span>
                             </Drawer.Item>
                             <Drawer.Item href="/calendar">
-                                <span className={location.pathname === "/calendar" ? 'nav-item-active' : ''}>Calendar</span>
+                                <span className={location.pathname === "/calendar" ? 'nav-item-active' : ''}>
+                                    <i className="fas fa-calendar"></i> Calendar
+                                </span>
                             </Drawer.Item>
                         </Drawer.Nav>
                     </Drawer.ToC>
